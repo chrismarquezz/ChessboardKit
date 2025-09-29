bcd740ca0144c6de5c1f04273f64020cf616726a
Chris <136855355+chrismarquezz@users.noreply.github.com>
2025-09-29 19:27:46 -0400

    Update resource path for ChessboardKit target

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .target(name: "ChessboardKit",
                 dependencies: ["ChessKit"],
                 resources: [
-                    .process("Assets/Pieces/uscf")
+                    .process("Assets/Pieces")
                 ])
     ],
     swiftLanguageModes: [.v5, .v6]
