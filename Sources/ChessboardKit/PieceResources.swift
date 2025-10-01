31cb9ccdc81819e5d760d22b489a2648da416ebd
Chris <136855355+chrismarquezz@users.noreply.github.com>
2025-10-01 17:41:28 -0400

    Update imageURL method to use subdirectory for assets

--- Sources/ChessboardKit/PieceResources.swift
+++ Sources/ChessboardKit/PieceResources.swift
@@ -2,6 +2,10 @@ import Foundation
 
 public struct PieceResources {
     public static func imageURL(theme: String, piece: String) -> URL? {
-        return Bundle.module.url(forResource: "\(theme)/\(piece)", withExtension: "png")
+        return Bundle.module.url(
+            forResource: piece,
+            withExtension: "png",
+            subdirectory: "Assets/Pieces/\(theme)"
+        )
     }
 }
