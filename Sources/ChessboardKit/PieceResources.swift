3c89294007fa5c3a4beabba6079ff0c7221fe5ea
Chris <136855355+chrismarquezz@users.noreply.github.com>
2025-10-01 17:47:08 -0400

    Delete Sources/ChessboardKit/PieceResources.swift

--- Sources/ChessboardKit/PieceResources.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public struct PieceResources {
-    public static func imageURL(theme: String, piece: String) -> URL? {
-        return Bundle.module.url(forResource: piece, withExtension: "png", subdirectory: theme)
-    }
-}
