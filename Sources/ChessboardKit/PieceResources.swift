9f21e2faa8cd0743c20bb5a60bccc4153ce6cf4a
Chris <136855355+chrismarquezz@users.noreply.github.com>
2025-10-01 17:44:00 -0400

    Simplify imageURL method for piece resources

--- Sources/ChessboardKit/PieceResources.swift
+++ Sources/ChessboardKit/PieceResources.swift
@@ -2,10 +2,6 @@ import Foundation
 
 public struct PieceResources {
     public static func imageURL(theme: String, piece: String) -> URL? {
-        return Bundle.module.url(
-            forResource: piece,
-            withExtension: "png",
-            subdirectory: "Assets/Pieces/\(theme)"
-        )
+        return Bundle.module.url(forResource: piece, withExtension: "png", subdirectory: theme)
     }
 }
